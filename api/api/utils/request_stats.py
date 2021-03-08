3c3fc78909e1765af08dd1951f81d03a656b00c7
Andy <andyle.uk13@gmail.com>
2021-03-08 13:23:21 +0100

    SC-11: Slightly updated documentation for counting requests module

--- api/api/utils/request_stats.py
+++ api/api/utils/request_stats.py
@@ -5,9 +5,9 @@ LOGGER = logging.getLogger("API")
 request_error_count = 0
 
 def track_request_error_count(func):
-    """Returns the decorated function for tracking the count of request for the error API."""
+    """Return the decorated function for tracking the count of request for the error API."""
     def wrapper():
-        """Returns function after counting and logging the number of requests."""
+        """Return function after counting and logging the number of requests."""
         global request_error_count
         request_error_count += 1
         LOGGER.info('Total requests for errors: %s', request_error_count)
