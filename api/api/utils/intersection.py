c91acef9e0dbf07f63b02a77a9daf8ef0e0d498c
Andy <andyle.uk13@gmail.com>
2021-03-08 13:21:49 +0100

    SC-12: Slightly updated documentation for intersection module

--- api/api/utils/intersection.py
+++ api/api/utils/intersection.py
@@ -1,14 +1,15 @@
+"""Module for intersecting data collections."""
 from typing import Any, Dict, Set
 
 def get_error_codes_as_sets(func):
-    """Executes the decorated function alongside its transformed parameters"""
+    """Return the decorated function."""
     def wrapper(*args):
-        """Returns the decorated function with transformed parameters"""
+        """Return the parameterised function with transformed parameters."""
         return func(*[set({error['code'] for error in error_dict}) for error_dict in args])
 
     return wrapper    
 
 @get_error_codes_as_sets
 def intersect_error_lists(error_list_a: Dict[str, Any], error_list_b: Dict[str, Any]) -> Set[int]:
-    """Return the intersect of both error dictionary parameters"""
+    """Return the intersect of both error dictionary parameters."""
     return error_list_a.intersection(error_list_b)
