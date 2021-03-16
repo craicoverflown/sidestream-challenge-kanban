7f2f551b6c4e3882c0e14c8e521b1107a51f765b
Andy <andyle.uk13@gmail.com>
2021-03-16 13:57:35 +0100

    SC-23: Error count added next to Error Group Header

---
+++ frontend/components/ErrorGroupHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="flex justify-between">
+    <h2 class="text-xl font-bold text-black mb-0.5">{{ headerTitle }}</h2>
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["headerTitle"]
+};
+</script>
